4e9ea5d7d9ddd33a9a4ce2c482f074008b72c861
hervster <hervenyemeck@gmail.com>
2020-07-20 16:22:44 -0400

    Moving py files to git root directory

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+# Import env variables
+load_dotenv()
+
+# Set up application and configuration
+app = Flask(__name__)
+app.config.from_object(os.environ.get('APP_SETTINGS'))
+print(os.environ.get('APP_SETTINGS'))
+
+@app.route('/')
+def hello():
+    return "What up my g?"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
